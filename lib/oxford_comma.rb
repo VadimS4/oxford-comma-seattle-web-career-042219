6cb80023d07f21c240e91abe5b1aefd4092141ee
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-12 22:42:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 3 
     array.prepend(" and ")
   elsif array.length > 3
-    array.prepend( "and")
+    array[last].prepend( "and")
   else
     array.join
   end
